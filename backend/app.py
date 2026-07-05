d614728ec7c9df986a353a99340595adec5ee07b
Hemang Bagadi <hemangbagadi77@gmail.com>
2026-07-05 17:04:20 +0530

    Prepared backend for Render deployment

--- backend/app.py
+++ backend/app.py
@@ -14,8 +14,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173"
-    ],
+    "http://localhost:5173",
+    "https://contentforge-ai.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
